59156aee916824dd6df33b2e928020a6cf5bb644
Tommy Li <Tommy.chaoping.li@ibm.com>
2017-04-21 13:23:02 -0700

    Update install.sh
    Added echo to prevent fails on travis.

--- install.sh
+++ install.sh
@@ -4,8 +4,11 @@ BXUSER=$user BXPASS=$password BXACCOUNT=1 ./linux.sh
 
 echo "y" | bx cs cluster-rm wordpress
 bx cs cluster-create --name wordpress
+echo "Creating cluster"
 sleep 5m
+echo "Creating cluster"
 sleep 5m
+echo "Creating cluster"
 sleep 5m
 bx cs workers wordpress
 bx cs cluster-config wordpress
